98fe09c9d67900cb29e2fd8def232a76893dc725
JenJetson <kari@jenjetson.com>
2020-04-16 10:03:14 -0700

    Changed the configurator to set background color, header/footer, and header text color only.

--- inc/customizer.php
+++ inc/customizer.php
@@ -11,36 +11,34 @@
  * @param WP_Customize_Manager $wp_customize Theme Customizer object.
  */
 function jetsy_customize_register( $wp_customize ) {
-	$wp_customize->get_setting( 'blogname' )->transport         = 'postMessage';
-	$wp_customize->get_setting( 'blogdescription' )->transport  = 'postMessage';
-	$wp_customize->get_setting( 'header_textcolor' )->transport = 'postMessage';
-	
-	/**
-	 * Custom Customizer Customizations
-	 */
-	
+    $wp_customize->get_setting( 'blogname' )->transport = 'postMessage';
+    $wp_customize->get_setting( 'blogdescription' )->transport  = 'postMessage';
+    $wp_customize->get_setting( 'header_textcolor' )->transport = 'postMessage';
 
-	//This is to control the header and footer background color.  I don't want for this right now.
-//	$wp_customize->add_setting( 'theme_bg_color', array(
-//		'default'			=> '#ff4500',
-//		'transport'			=> 'postMessage',
-//		'type'				=> 'theme_mod',
-//		'sanitize_callback' => 'sanitize_hex_color',
-//	));
-//
-//	// Control for header and footer background color.
-//	$wp_customize->add_control(
-//		new WP_Customize_Color_Control(
-//			$wp_customize,
-//			'theme_bg_color',
-//				array(
-//					'label'		=> __( 'Header and footer background color', 'jetsy'),
-//					'section'	=> 'colors',
-//					'settings'	=> 'theme_bg_color'
-//				)
-//		)
-//	);
-	
+
+
+//	This is to control the header and footer background color.
+    $wp_customize->add_setting( 'theme_bg_color', array(
+        'default'			=> '#ff4500',  //default color
+        'transport'			=> 'postMessage',
+        'type'				=> 'theme_mod',  //type of setting
+        'sanitize_callback' => 'sanitize_hex_color',  //use anytime user types in code
+    ));
+
+    // Control for header and footer background color.
+    $wp_customize->add_control(
+        new WP_Customize_Color_Control(  //created the interactive color control
+            $wp_customize,
+            'theme_bg_color', //where sending changes
+            array(
+                'label'		=> __( 'Header / footer background color', 'jetsy'),
+                'section'	=> 'colors',  //what section added to
+                'settings'	=> 'theme_bg_color'  //control name
+            )
+        )
+    );
+
+
 //	// Create interactive color setting
 //	$wp_customize->add_setting( 'interactive_color' ,
 //		array(
@@ -51,7 +49,7 @@ function jetsy_customize_register( $wp_customize ) {
 //			'transport'			=> 'postMessage',
 //		)
 //	);
-	
+
 //	// Add the controls
 //	$wp_customize->add_control(
 //		new WP_Customize_Color_Control(
@@ -63,41 +61,41 @@ function jetsy_customize_register( $wp_customize ) {
 //			)
 //		)
 //	);
-	
-	// Add option to select index content
-	$wp_customize->add_section( 'theme_options', 
-		array(
-			'title'			=> __( 'Theme Options', 'jetsy' ),
-			'priority'		=> 95,
-			'capability'	=> 'edit_theme_options',
-			'description'	=> __( 'Change how much of a post is displayed on index and archive pages.', 'jetsy' )
-		)
-	);
-	
-	// Create excerpt or full content settings
-	$wp_customize->add_setting(	'length_setting',
-		array(
-			'default'			=> 'excerpt',
-			'type'				=> 'theme_mod',
-			'sanitize_callback' => 'jetsy_sanitize_length', // Sanitization function appears further down
-			'transport'			=> 'postMessage'
-		)
-	);
 
-	// Add the controls
-	$wp_customize->add_control(	'jetsy_length_control',
-		array(
-			'type'		=> 'radio',
-			'label'		=> __( 'Index/archive displays', 'jetsy' ),
-			'section'	=> 'theme_options',
-			'choices'	=> array(
-				'excerpt'		=> __( 'Excerpt (default)', 'jetsy' ),
-				'full-content'	=> __( 'Full content', 'jetsy' )
-			),
-			'settings'	=> 'length_setting' // Matches setting ID from above
-		)
-	);
-	
+    // Add option to select index content
+    $wp_customize->add_section( 'theme_options',
+        array(
+            'title'			=> __( 'Theme Options', 'jetsy' ),
+            'priority'		=> 95,
+            'capability'	=> 'edit_theme_options',
+            'description'	=> __( 'Change how much of a post is displayed on index and archive pages.', 'jetsy' )
+        )
+    );
+
+    // Create excerpt or full content settings
+    $wp_customize->add_setting(	'length_setting',
+        array(
+            'default'			=> 'excerpt',
+            'type'				=> 'theme_mod',
+            'sanitize_callback' => 'jetsy_sanitize_length', // Sanitization function appears further down
+            'transport'			=> 'postMessage'
+        )
+    );
+
+    // Add the controls
+    $wp_customize->add_control(	'jetsy_length_control',
+        array(
+            'type'		=> 'radio',
+            'label'		=> __( 'Index/archive displays', 'jetsy' ),
+            'section'	=> 'theme_options',
+            'choices'	=> array(
+                'excerpt'		=> __( 'Excerpt (default)', 'jetsy' ),
+                'full-content'	=> __( 'Full content', 'jetsy' )
+            ),
+            'settings'	=> 'length_setting' // Matches setting ID from above
+        )
+    );
+
 }
 add_action( 'customize_register', 'jetsy_customize_register' );
 
@@ -105,7 +103,7 @@ add_action( 'customize_register', 'jetsy_customize_register' );
  * Binds JS handlers to make Theme Customizer preview reload changes asynchronously.
  */
 function jetsy_customize_preview_js() {
-	wp_enqueue_script( 'jetsy_customizer', get_template_directory_uri() . '/js/customizer.js', array( 'customize-preview' ), '20151215', true );
+    wp_enqueue_script( 'jetsy_customizer', get_template_directory_uri() . '/js/customizer.js', array( 'customize-preview' ), '20151215', true );
 }
 add_action( 'customize_preview_init', 'jetsy_customize_preview_js' );
 
@@ -119,161 +117,160 @@ add_action( 'customize_preview_init', 'jetsy_customize_preview_js' );
 function jetsy_sanitize_length( $value ) {
     if ( ! in_array( $value, array( 'excerpt', 'full-content' ) ) ) {
         $value = 'excerpt';
-	}
+    }
     return $value;
 }
 
 
 if ( ! function_exists( 'jetsy_header_style' ) ) :
-/**
- * Styles the header image and text displayed on the blog.
- *
- * @see jetsy_custom_header_setup().
- */
-function jetsy_header_style() {
-	$header_text_color = get_header_textcolor();
-	//disabled control of background color.
-	//$header_bg_color = get_theme_mod( 'theme_bg_color' );
-	$interactive_color = get_theme_mod('interactive_color');
+    /**
+     * Styles the header image and text displayed on the blog.
+     *
+     * @see jetsy_custom_header_setup().
+     */
+    function jetsy_header_style() {
+        $header_text_color = get_header_textcolor();
+        $header_bg_color = get_theme_mod( 'theme_bg_color' );
+        $interactive_color = get_theme_mod('interactive_color');
 
-	/*
-	 * If no custom options for text are set, let's bail.
-	 * get_header_textcolor() options: Any hex value, 'blank' to hide text. Default: HEADER_TEXTCOLOR.
-	 */
-	if ( HEADER_TEXTCOLOR != $header_text_color ) {
+        /*
+         * If no custom options for text are set, let's bail.
+         * get_header_textcolor() options: Any hex value, 'blank' to hide text. Default: HEADER_TEXTCOLOR.
+         */
+        if ( HEADER_TEXTCOLOR != $header_text_color ) {
 
-		// If we get this far, we have custom styles. Let's do this.
-		?>
-		<style type="text/css">
-		<?php
-			// Has the text been hidden?
-			if ( ! display_header_text() ) :
-		?>
-			.site-title,
-			.site-description {
-				position: absolute;
-				clip: rect(1px, 1px, 1px, 1px);
-			}
-		<?php
-			// If the user has set a custom color for the text use that.
-			else :
-		?>
-			.site-title a,
-			.site-description {
-				color: #<?php echo esc_attr( $header_text_color ); ?>;
-			}
-		<?php endif; ?>
-			.main-navigation a,
-			button.dropdown-toggle,
-			.menu-toggle,
-			.site-footer,
-			.site-footer a {
-				color: #<?php echo esc_attr( $header_text_color ); ?>;
-			}
+            // If we get this far, we have custom styles. Let's do this.
+            ?>
+            <style type="text/css">
+                <?php
+                    // Has the text been hidden?
+                    if ( ! display_header_text() ) :
+                ?>
+                .site-title,
+                .site-description {
+                    position: absolute;
+                    clip: rect(1px, 1px, 1px, 1px);
+                }
+                <?php
+                    // If the user has set a custom color for the text use that.
+                    else :
+                ?>
+                .site-title a,
+                .site-description {
+                    color: #<?php echo esc_attr( $header_text_color ); ?>;
+                }
+                <?php endif; ?>
+                .main-navigation a,
+                button.dropdown-toggle,
+                .menu-toggle,
+                .site-footer,
+                .site-footer a {
+                    color: #<?php echo esc_attr( $header_text_color ); ?>;
+                }
 
-			.menu-toggle,
-			.custom-logo-link:focus > img, .custom-logo-link:hover > img {
-				outline-color: #<?php echo esc_attr( $header_text_color ); ?>;
-			}
+                .menu-toggle,
+                .custom-logo-link:focus > img, .custom-logo-link:hover > img {
+                    outline-color: #<?php echo esc_attr( $header_text_color ); ?>;
+                }
 
-			button.menu-toggle:hover,
-			button.menu-toggle:focus {
-				/*color: */<?php //echo esc_attr( $header_bg_color ); ?>/*;*/
-				/*background-color: #*/<?php //echo esc_attr( $header_text_color ); ?>/*;*/
-			}
-		</style>
-		<?php
-	}
-	
-	/*
-	 * Do we have a custom header background color?
-	 */
-	if ( '#FF4500' != $header_bg_color ) { ?>
-		<style type="text/css">
-			.site-header,
-			.site-footer {
-				background-color: <?php echo esc_attr( $header_bg_color ); ?>;
-			}
-		</style>
-	<?php
-	}
-	
-	/*
-	 * Do we have a custom interactive color?
-	 */
-	if ( '#FFA500' != $interactive_color ) { ?>
-		<style type="text/css">
-			a:hover,
-			a:focus, 
-			a:active,
-			.page-content a:focus, .page-content a:hover,
-			.entry-content a:focus,
-			.entry-content a:hover,
-			.entry-summary a:focus,
-			.entry-summary a:hover,
-			.comment-content a:focus,
-			.comment-content a:hover,
-			.cat-links a {
-				color: <?php echo esc_attr( $interactive_color ); ?>;
-			}
-			
-			.page-content a,
-			.entry-content a,
-			.entry-summary a,
-			.comment-content a,
-			.post-navigation .post-title,
-			.comment-navigation a:hover, 
-			.comment-navigation a:focus,
-			.posts-navigation a:hover,
-			.posts-navigation a:focus,
-			.post-navigation a:hover,
-			.post-navigation a:focus,
-			.paging-navigation a:hover,
-			.paging-navigation a:focus,
-			.entry-title a:hover, 
-			.entry-title a:focus,
-			.entry-meta a:focus, 
-			.entry-meta a:hover,
-			.entry-footer a:focus,
-			.entry-footer a:hover,
-			.reply a:hover, 
-			.reply a:focus,
-			.comment-form .form-submit input:hover, 
-			.comment-form .form-submit input:focus,
-			.widget a:hover, 
-			.widget a:focus {
-				border-color: <?php echo esc_attr( $interactive_color ); ?>;
-			}
-			
-			.comment-navigation a:hover, 
-			.comment-navigation a:focus,
-			.posts-navigation a:hover,
-			.posts-navigation a:focus,
-			.post-navigation a:hover,
-			.post-navigation a:focus,
-			.paging-navigation a:hover,
-			.paging-navigation a:focus,
-			.continue-reading a:focus, 
-			.continue-reading a:hover,
-			.cat-links a:focus, 
-			.cat-links a:hover,
-			.reply a:hover, 
-			.reply a:focus,
-			.comment-form .form-submit input:hover, 
-			.comment-form .form-submit input:focus {
-				background-color: <?php echo esc_attr( $interactive_color ); ?>;
-			}
-			
-			@media screen and (min-width: 900px) {
-				.no-sidebar .post-content__wrap .entry-meta a:hover, 
-				.no-sidebar .post-content__wrap .entry-meta a:focus {
-					border-color: <?php echo esc_attr( $interactive_color ); ?>;
-				}
-			}
-		</style>
-	<?php
-	} 
-}
+                button.menu-toggle:hover,
+                button.menu-toggle:focus {
+                    color: <?php echo esc_attr( $header_bg_color ); ?>;
+                    background-color: #<?php echo esc_attr( $header_text_color ); ?>;
+                }
+            </style>
+            <?php
+        }
+
+        /*
+         * Do we have a custom header background color?
+         */
+        if ( '#ff4500' != $header_bg_color) { ?>
+            <style type="text/css">
+                .site-header,
+                .site-footer {
+                    background-color: <?php echo esc_attr( $header_bg_color ); ?>;
+                }
+            </style>
+            <?php
+        }
+
+        /*
+         * Do we have a custom interactive color?
+         */
+        if ( '#FFA500' != $interactive_color ) { ?>
+            <style type="text/css">
+                a:hover,
+                a:focus,
+                a:active,
+                .page-content a:focus, .page-content a:hover,
+                .entry-content a:focus,
+                .entry-content a:hover,
+                .entry-summary a:focus,
+                .entry-summary a:hover,
+                .comment-content a:focus,
+                .comment-content a:hover,
+                .cat-links a {
+                    color: <?php echo esc_attr( $interactive_color ); ?>;
+                }
+
+                .page-content a,
+                .entry-content a,
+                .entry-summary a,
+                .comment-content a,
+                .post-navigation .post-title,
+                .comment-navigation a:hover,
+                .comment-navigation a:focus,
+                .posts-navigation a:hover,
+                .posts-navigation a:focus,
+                .post-navigation a:hover,
+                .post-navigation a:focus,
+                .paging-navigation a:hover,
+                .paging-navigation a:focus,
+                .entry-title a:hover,
+                .entry-title a:focus,
+                .entry-meta a:focus,
+                .entry-meta a:hover,
+                .entry-footer a:focus,
+                .entry-footer a:hover,
+                .reply a:hover,
+                .reply a:focus,
+                .comment-form .form-submit input:hover,
+                .comment-form .form-submit input:focus,
+                .widget a:hover,
+                .widget a:focus {
+                    border-color: <?php echo esc_attr( $interactive_color ); ?>;
+                }
+
+                .comment-navigation a:hover,
+                .comment-navigation a:focus,
+                .posts-navigation a:hover,
+                .posts-navigation a:focus,
+                .post-navigation a:hover,
+                .post-navigation a:focus,
+                .paging-navigation a:hover,
+                .paging-navigation a:focus,
+                .continue-reading a:focus,
+                .continue-reading a:hover,
+                .cat-links a:focus,
+                .cat-links a:hover,
+                .reply a:hover,
+                .reply a:focus,
+                .comment-form .form-submit input:hover,
+                .comment-form .form-submit input:focus {
+                    background-color: <?php echo esc_attr( $interactive_color ); ?>;
+                }
+
+                @media screen and (min-width: 900px) {
+                    .no-sidebar .post-content__wrap .entry-meta a:hover,
+                    .no-sidebar .post-content__wrap .entry-meta a:focus {
+                        border-color: <?php echo esc_attr( $interactive_color ); ?>;
+                    }
+                }
+            </style>
+            <?php
+        }
+    }
 endif;
 
 
